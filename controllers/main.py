d4f021ee2af7522fbca05cffc16ebe8b47145f27
José David <jcabmar2012@g.educaand.es>
2026-01-29 10:15:14 +0100

    Carpeta Controller configurada

---
+++ controllers/main.py
@@ -0,0 +1,14 @@
+from odoo import http
+from odoo.http import request
+class ElectroWordFrontend(http.Controller):
+    @http.route('/products_page', type='http', auth='public', website=True)
+    def products_page(self):
+        """ Muestra una página con productos en el sitio web """
+        products = request.env['product.template'].sudo().search([])
+        return request.render('electroword_api.products_template', {'products': products})
+
+    @http.route('/customers_page', type='http', auth='public', website=True)
+    def customers_page(self):
+        """ Muestra una página con clientes en el sitio web """
+        customers = request.env['res.partner'].sudo().search([('customer_rank', '>', 0)])
+        return request.render('electroword_api.customers_template', {'customers': customers})
