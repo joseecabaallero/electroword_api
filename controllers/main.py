a7dc5ff7f0f21aff31aa7997b06e90296f369625
José David <jcabmar2012@g.educaand.es>
2026-02-10 10:53:10 +0100

    Corrección

--- controllers/main.py
+++ controllers/main.py
@@ -6,11 +6,11 @@ class ElectroWordFrontend(http.Controller):
     @http.route('/products_page', type='http', auth='public', website=True)
     def products_page(self):
         """ Muestra una página con productos en el sitio web """
-        products = request.env['product.template'].sudo().search([])
+        products = request.env['product.template'].sudo().search([], limit=20)
         return request.render('electroword_api.products_template', {'products': products})
 
     @http.route('/customers_page', type='http', auth='public', website=True)
     def customers_page(self):
         """ Muestra una página con clientes en el sitio web """
-        customers = request.env['res.partner'].sudo().search([('customer_rank', '>', 0)])
+        customers = request.env['res.partner'].sudo().search([('customer_rank', '>', 0)], limit=20)
         return request.render('electroword_api.customers_template', {'customers': customers})
